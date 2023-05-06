4532a4da8116626bd9838f8b4a3cc3fc8ff9e6ce
Huszar Istvan <44548937+xDeimen@users.noreply.github.com>
2023-05-06 23:43:46 +0300

    Lab3_Ex3

---
+++ Lab3/Exercises/Ex3/Ex3.2.py
@@ -0,0 +1,21 @@
+from csv import writer
+from csv import reader
+
+
+director = "S:\III\Sem II\AI\AI_Labs_Python\Lab3\Exercises\Ex3"
+fisier_1 = "\citire.csv"
+fisier_2 = "\scriere.csv"
+
+def add_column_in_csv(cale_c, cale_s, transform_row):
+    with open(cale_c, 'r') as citire, open(cale_s, 'w', newline="") as scriere:
+        csv_citire = reader(citire)
+        csv_scriere = writer(scriere)
+
+        for linie in csv_citire:
+            transform_row(linie, csv_citire.line_num)
+            csv_scriere.writerow(linie)
+
+cale_1 = director + fisier_1
+cale_2 = director + fisier_2
+list_of_str = ['Observatii', 'Python', 'Qualica', 'Delmia', 'PLC']
+add_column_in_csv(cale_1,cale_2, lambda linie, line_num: linie.append(list_of_str[line_num-1]))
